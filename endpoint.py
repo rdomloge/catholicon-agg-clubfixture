8aece4774198dc6228ccdeeb667971d460bbfc52
Ramsay.Domloge <rdomloge@gmail.com>
2023-07-25 15:17:44 +0100

    Lots of changes to get the first working version

--- endpoint.py
+++ endpoint.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
 from datetime import datetime
 
-import requests, logging
+import requests, logging, os
 
 #Configure logging
 logging.basicConfig()
-logging.root.setLevel(logging.DEBUG)
+LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+logging.basicConfig(level=LOGLEVEL)
+#logging.root.setLevel(logging.DEBUG)
 
-HOSTPORT = "rdomloge.entrydns.org:80"
-FIXTURE_URL_TEMPLATE = "http://"+HOSTPORT+"/fixtures/search/findByExternalFixtureId?externalFixtureId="
-CLUBS_URL_TEMPLATE = "http://"+HOSTPORT+"/clubs/search/findClubByTeamId?teamId="
+FIXTURE_URL_TEMPLATE = "http://catholicon-ms-matchcard-service/fixtures/search/findByExternalFixtureId?externalFixtureId="
+CLUBS_URL_TEMPLATE = "http://catholicon-ms-club-service/clubs/search/findClubByTeamId?teamId="
 
 fixtureDateFormat = "%Y-%m-%dT%H:%M:%SZ"
 
@@ -17,17 +18,13 @@ app = Flask(__name__)
 
 def fetchFixture(fixtureId):
     response = requests.get(FIXTURE_URL_TEMPLATE+str(fixtureId))
-    if response.status_code == 200:
-        logging.info('Got this response for the fixture: '+response.text)
-        return response.json()
+    if response.status_code == 200: return response.json()        
     else:
         logging.warning('Response code was '+str(response.status_code))
 
 def fetchClub(teamId):
     response = requests.get(CLUBS_URL_TEMPLATE+str(teamId))
-    if response.status_code == 200:
-        logging.info('Got this response for the club: '+response.text)
-        return response.json()
+    if response.status_code == 200: return response.json()
     else:
         logging.warning('Response code was '+str(response.status_code))
 
@@ -52,13 +49,12 @@ def fetchMatchSessionForFixture(homeTeamId, matchDate):
         if weekDayConversion(sesh["days"]) == matchDayOfWeek: return sesh
             
 
-@app.route('/', methods=['GET'])
+@app.route('/clubfixture', methods=['GET'])
 def index():
     if request.args.get('fixtureId') == None:
         return 'Error - please specify fixtureId'
     fixtureId = request.args['fixtureId']
     fixture = fetchFixture(fixtureId=fixtureId)
-    print(str(type(fixture)))
     homeTeamId = fixture["homeTeamId"]
     matchDate = fixture["matchDate"]
     matchSession = fetchMatchSessionForFixture(homeTeamId=homeTeamId, matchDate=matchDate)
@@ -69,5 +65,5 @@ def index():
     return jsonResponse
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0') 
+    app.run(debug=False, host='0.0.0.0') 
 
